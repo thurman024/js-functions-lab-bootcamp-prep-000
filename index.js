4270a4c1d8d2d85daeb04e8c0b31e430191b1603
Stephen Thurman <thurman024@gmail.com>
2017-11-25 01:48:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 // write your code below!
 function happyHolidays() {
-  return "Happy Holidays!"
+  return "Happy holidays!"
 }
 
 function happyHolidaysTo(name) {
-  return(`Happy Holidays, ${name}`)
+  return(`Happy holidays, ${name}`)
+}
+
+function happyHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`
 }
